449f4e29080179b32f4d13d1aa0e3d37d112d776
RaviBabu-coder <56721579+RaviBabu-coder@users.noreply.github.com>
2019-10-18 15:58:57 +0530

    Update HowToUse.cs

--- C-Sharp/HowToUse.cs
+++ C-Sharp/HowToUse.cs
@@ -4,7 +4,7 @@
 			CryptLib _crypt = new CryptLib ();
 			string plainText = "This is the text to be encrypted";
 			String iv = CryptLib.GenerateRandomIV (16); //16 bytes = 128 bits
-			string key = CryptLib.getHashSha256("my secret key", 32); //32 bytes = 256 bits
+			string key = CryptLib.getHashSha256("my secret key", 32);//APMT TERMPoint Mobile SecuritKey HashCode = 8191 //32 bytes = 256 bits
 			String cypherText = _crypt.encrypt (plainText, key, iv);
 			Console.WriteLine ("iv="+iv);
 			Console.WriteLine ("key=" + key);
